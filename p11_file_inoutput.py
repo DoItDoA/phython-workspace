3dd6f590b399686cec280c5120bccb9fc451e2a6
DongA Shin <82761686+DoItDoA@users.noreply.github.com>
2022-06-20 10:49:25 +0900

    Update p11_file_inoutput.py

--- p11_file_inoutput.py
+++ p11_file_inoutput.py
@@ -4,7 +4,7 @@ print("영어 : 50", file=score_file) # score.txt 파일에 내용 쓰기
 score_file.close() # score.txt 파일 닫기
 
 
-score_file = open("score.txt", "a", encoding="utf8") # score.txt 파일을 쓰기("a") 모드로 열기
+score_file = open("score.txt", "a", encoding="utf8") # score.txt 파일을 내용추가("a") 모드로 열기
 score_file.write("과학 : 80")
 score_file.write("\n코딩 : 100") # write 는 줄바꿈 안해주기 때문에 탈출문자(\n)로 줄바꿈 추가
 score_file.close()
